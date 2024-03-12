9f71b0d16a6ba1f1cdda08b8660c3110f97b63b3
Karansure <146506062+Karansure@users.noreply.github.com>
2024-03-12 11:13:15 +0530

    password_generation.py

--- password_generation.py
+++ password_generation.py
@@ -1,13 +1,14 @@
 import random 
 import pyperclip
-sl=int(input('enter the password length: '))
-x='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-s='abcdefghijklmnopqrstuvwxyz'
-t=123456789
-w='!@#$%^&*'
-p=x+s+str(t)+w
 
-password = "".join (random.sample(p,sl))
+length=int(input('enter the password length: '))
+upper_case='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+lower_case='abcdefghijklmnopqrstuvwxyz'
+number=123456789
+special_characters='!@#$%^&*'
+con=upper_case+lower_case+str(number)+special_characters
+
+password = "".join (random.sample(con,length))
 pyperclip.copy(password)
 
 print('your passowrd is ',password)
